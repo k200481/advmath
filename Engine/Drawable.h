435a1b065c000a88af5fa60cd12c31248985f756
Sarim <msaarimkh@gmail.com>
2021-07-04 01:01:45 +0500

    Drawable uses reference to model instead of value

--- Engine/Drawable.h
+++ Engine/Drawable.h
@@ -7,10 +7,10 @@
 class Drawable
 {
 public:
-	Drawable( std::vector<Vec2> model,Color c )
+	Drawable( const std::vector<Vec2>& model,Color c )
 		:
 		c( c ),
-		model( std::move( model ) )
+		model( model )
 	{}
 	void Translate( const Vec2& translation_in )
 	{
@@ -31,17 +31,46 @@ public:
 	}
 	void Render( Graphics& gfx )
 	{
-		for( auto& v : model )
+		/*auto modelCpy = model;
+		for (auto& v : modelCpy)
 		{
 			v.x *= scale_x;
 			v.y *= scale_y;
 			v += translation;
 		}
-		gfx.DrawClosedPolyline( model,c );
+		gfx.DrawClosedPolyline(modelCpy, c);*/
+
+		for (auto i = model.begin(); i != std::prev(model.end()); i++)
+		{
+			auto v = *i;
+			auto vNext = *std::next(i);
+
+			v.x *= scale_x;
+			v.y *= scale_y;
+			v += translation;
+
+			vNext.x *= scale_x;
+			vNext.y *= scale_y;
+			vNext += translation;
+
+			gfx.DrawLine( v, vNext, c );
+		}
+		auto v = *std::prev(model.end());
+		auto vNext = *model.begin();
+
+		v.x *= scale_x;
+		v.y *= scale_y;
+		v += translation;
+
+		vNext.x *= scale_x;
+		vNext.y *= scale_y;
+		vNext += translation;
+
+		gfx.DrawLine(v, vNext, c);
 	}
 private:
 	Color c;
-	std::vector<Vec2> model;
+	const std::vector<Vec2>& model;
 	Vec2 translation = {0.0f,0.0f};
 	float scale_x = 1.0f;
 	float scale_y = 1.0f;
