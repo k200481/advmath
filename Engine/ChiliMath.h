4f1e701c1d3b60bc87ae17edfa2f48a7145e41c9
Sarim <msaarimkh@gmail.com>
2021-07-06 00:09:34 +0500

    Defined PI

--- Engine/ChiliMath.h
+++ Engine/ChiliMath.h
@@ -2,6 +2,8 @@
 #include <cmath>
 #include "Vec2.h"
 
+#define PI 3.1418
+
 template<typename T>
 auto sq( T x )
 {
@@ -16,4 +18,18 @@ T DistancePointLine( const _Vec2<T>& p0,const _Vec2<T>& p1,const _Vec2<T>& q )
 	const T c = p0.x * p1.y - p1.x * p0.y;
 
 	return std::abs( a * q.x + b * q.y + c ) / std::sqrt( sq( a ) + sq( b ) );
+}
+
+template<typename T>
+_Vec2<T> PointOnPerpendicularBisector( const _Vec2<T>& p0, const _Vec2<T>& p1, const _Vec2<T>& pt )
+{
+	T m = ( p0.y - p1.y ) / ( p0.x - p1.x );
+	T w = -1 / m;
+
+	T c = p0.y - m * p0.x;
+	T b = pt.y - w * pt.x;
+
+	T x = ( b - c ) / ( m - w );
+	T y = m * x + c;
+	return { x, y };
 }
